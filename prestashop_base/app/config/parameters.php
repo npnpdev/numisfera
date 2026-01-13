8330d8993337442b93cd15b737c8f78acd55bb55
npnpdev <npnpdev@gmail.com>
2026-01-13 18:45:27 +0100

    prawidlowe credentials do bd

--- prestashop_base/app/config/parameters.php
+++ prestashop_base/app/config/parameters.php
@@ -1,11 +1,11 @@
 <?php return array (
   'parameters' => 
   array (
-    'database_host' => 'mysql',
+    'database_host' => '10.40.71.115',
     'database_port' => '3306',
-    'database_name' => 'prestashop',
+    'database_name' => 'BE_194103',
     'database_user' => 'root',
-    'database_password' => 'prestashop',
+    'database_password' => 'student',
     'database_prefix' => 'ps_',
     'database_engine' => 'InnoDB',
     'mailer_transport' => 'smtp',
